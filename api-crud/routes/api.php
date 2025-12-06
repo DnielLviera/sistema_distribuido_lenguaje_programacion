20dde4ea23d2249a771ef9770eca614062a96d02
Andres Arevalo <andreseduardoarevalo.sul@gmail.com>
2025-12-05 23:07:56 -0400

    actualizacion del BE Laravel

---
+++ api-crud/routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\productController;
+
+Route::get('products', [productController::class, 'index']);
+Route::get('products/{id}', [productController::class, 'show']);
+
+// Route::post('products', [productController::class, 'store']);
+// Route::put('products/{id}', [productController::class, 'update']);
+// Route::patch('products/{id}', [productController::class, 'updatePartial']);
+// Route::delete('products/{id}', [productController::class, 'delete']);
+
+// Rutas Protegidas (Solo con Token JWT válido)
+Route::middleware(['jwt.verify'])->group(function () {
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::patch('products/{id}', [productController::class, 'updatePartial']);
+    Route::delete('products/{id}', [productController::class, 'delete']);
+});
